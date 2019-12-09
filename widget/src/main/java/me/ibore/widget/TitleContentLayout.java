ad0981207b7d4514b99248ea6e6584c85668e82a
Ibore Xie <bore521@live.com>
2019-12-09 10:57:40 +0800

    Initial commit

---
+++ widget/src/main/java/me/ibore/widget/TitleContentLayout.java
@@ -0,0 +1,20 @@
+package me.ibore.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class TitleContentLayout extends ConstraintLayout {
+    public TitleContentLayout(Context context) {
+        super(context);
+    }
+
+    public TitleContentLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleContentLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ widget/src/main/java/me/ibore/widget/TitleInputLayout.java
@@ -0,0 +1,23 @@
+package me.ibore.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class TitleInputLayout extends ConstraintLayout {
+
+    public TitleInputLayout(Context context) {
+        super(context);
+    }
+
+    public TitleInputLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TitleInputLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
